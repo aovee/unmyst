e5344c977cf7335ccbc7b9e580265ace1653cfaf
Ghislain "Ao" Linais <ghis.linais@gmail.com>
2026-06-27 17:18:56 +0200

    refactor(login): update login page card UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,24 @@ import { signIn } from '@/auth'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 
 export default function LoginPage() {
   return (
-    <main className="p-8 max-w-sm mx-auto">
-      <Card>
+    <main className="h-screen flex items-center">
+      <Card className="w-full max-w-sm mx-auto">
         <CardHeader>
           <CardTitle>Sign in</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <form
@@ -32,9 +42,13 @@ export default function LoginPage() {
                 placeholder="you@example.com"
               />
             </div>
-            <Button type="submit">Send magic link</Button>
           </form>
         </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Send magic link
+          </Button>
+        </CardFooter>
       </Card>
     </main>
   )
