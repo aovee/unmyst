c4a5a3f27d4996d32dd2f17272e9c1ce02df70b1
Aovee <6479304+aovee@users.noreply.github.com>
2026-06-27 10:43:27 +0200

    Initial commit

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from 'prettier'
+
+const config: Config = {
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  ignorePatterns: [],
+  printWidth: 80
+}
+
+export default config
