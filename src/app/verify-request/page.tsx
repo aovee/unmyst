41ea995f4d8fef04f4657cb27880589fe72d4be8
Aovee <6479304+aovee@users.noreply.github.com>
2026-06-29 14:27:00 +0200

    feat: Redesign verify request page with new branding

--- src/app/verify-request/page.tsx
+++ src/app/verify-request/page.tsx
@@ -1,23 +1,35 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
+import Link from 'next/link'
+
+import { Logo } from '@/components/logo'
+import { FieldDescription, FieldGroup } from '@/components/ui/field'
 
 export default function VerifyRequestPage() {
   return (
-    <main className="h-screen flex items-center">
-      <Card className="w-full max-w-sm mx-auto">
-        <CardHeader>
-          <CardTitle>Success</CardTitle>
-          <CardDescription>An email has been sent to you</CardDescription>
-        </CardHeader>
-        <CardContent>
-          Please check your inbox and click the link to sign in
-        </CardContent>
-      </Card>
-    </main>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <FieldGroup>
+            <div className="flex flex-col items-center gap-2">
+              <Link href="/">
+                <Logo />
+              </Link>
+              <div className="flex flex-col items-center gap-4 text-center">
+                <h1 className="text-xl font-bold">Verify your email</h1>
+                <FieldDescription>
+                  An activation link has been sent to your email address. Please
+                  check your inbox and click on the link to complete the
+                  activation process.
+                </FieldDescription>
+              </div>
+              <div className="mt-5">
+                <FieldDescription>
+                  Didn&apos;t get the mail? <a href="#">Resend</a>
+                </FieldDescription>
+              </div>
+            </div>
+          </FieldGroup>
+        </div>
+      </div>
+    </div>
   )
 }
