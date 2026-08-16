86512768d9b2fda6ac9322b9e321507d7fc9489a
Aovee <6479304+aovee@users.noreply.github.com>
2026-08-16 17:43:12 +0200

    Style: Make header height responsive and adjust toolbar padding

--- app/components/DataToolbarCard.vue
+++ app/components/DataToolbarCard.vue
@@ -9,7 +9,7 @@ defineProps<{
 
 <template>
   <div
-    class="ring ring-default w-full py-4 px-8 flex flex-col gap-3 min-w-2xs mb-px"
+    class="ring ring-default w-full py-4 px-6 flex flex-col gap-3 min-w-2xs mb-px"
     :class="fullWidth ? 'col-span-1 sm:col-span-2 lg:col-span-4' : ''"
   >
     <slot name="content">
